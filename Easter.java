80e87b96bd6ed0344f96ec4bf3d9403777c05deb
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2025-09-03 15:05:09 +0000

    Working on it

--- Easter.java
+++ Easter.java
@@ -1,5 +1,6 @@
-public class Easter{
-    public static void main(String[] args){
-
+public class Easter {
+    public static void main(String[] args) {
+        int y = 2003;
+        a = y%19;
     }
 }
